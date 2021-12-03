5b512fee0eecafb6a6ade0af1f4858b6f0524aa3
Miles Bardon <milesbardon@gmail.com>
2021-12-03 11:38:06 +0000

    Completing Day 3

--- AdventOfCode.Tests/Day_03Test.cs
+++ AdventOfCode.Tests/Day_03Test.cs
@@ -6,12 +6,45 @@ namespace AdventOfCode.Tests
     {
         Day_03 day = new Day_03();
 
+        string[] input = new string[] {
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        };
+
+        [Fact]
+        public void Example1_ReturnsAnswer()
+        {
+            var result = day.CalculateGammaAndEpsilonRate(input);
+
+            Assert.Equal(22, result.Item1);
+            Assert.Equal(9, result.Item2);
+        }
+
+        [Fact]
+        public void Example2_ReturnsAnswer()
+        {
+            var result = day.CalculateOxygenAndCO2Rating(input);
+
+            Assert.Equal(23, result.Item1);
+            Assert.Equal(10, result.Item2);
+        }
+
         [Fact]
         public void Solution1_ReturnsAnswer()
         {
             var result = day.Solve_1().ToString();
 
-            Assert.Equal("Solution 1", result);
+            Assert.Equal("738234", result);
         }
 
         [Fact]
@@ -19,7 +52,7 @@ namespace AdventOfCode.Tests
         {
             var result = day.Solve_2().ToString();
 
-            Assert.Equal("Solution 2", result);
+            Assert.Equal("3969126", result);
         }
     }
 }
--- AdventOfCode/Day_03.cs
+++ AdventOfCode/Day_03.cs
@@ -4,15 +4,98 @@ namespace AdventOfCode
 {
     public class Day_03 : BaseDay
     {
-        private readonly string _input;
+        private readonly string[] _input;
 
         public Day_03()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = File.ReadAllText(InputFilePath).Split('\n');
         }
 
-        public override ValueTask<string> Solve_1() => new("Solution 1");
+        public (int, int) CalculateGammaAndEpsilonRate(string[] input)
+        {
+            string gamma = "";
+            string epsilon = "";
 
-        public override ValueTask<string> Solve_2() => new("Solution 2");
+            int codeLength = input[0].Length;
+
+            for (int i = 0; i < codeLength; i++)
+            {
+                var transposed = "";
+
+                foreach (var code in input)
+                {
+                    transposed += code[i];
+                }
+
+                int oneCount = transposed.Count(f => f == '1');
+                int transposedLength = transposed.Length;
+
+                if (oneCount > transposedLength / 2)
+                {
+                    gamma += "1";
+                    epsilon += "0";
+                }
+                else
+                {
+                    gamma += "0";
+                    epsilon += "1";
+                }
+            }
+
+            return (Convert.ToInt32(gamma, 2), Convert.ToInt32(epsilon, 2));
+        }
+
+        private int CalculateLifeSupportRating(string[] originalInput, Func<int, int, bool> predicate)
+        {
+            int codeLength = originalInput[0].Length;
+            string[] currentInput = originalInput;
+
+            // Go throw each digit in the code
+            for (int i = 0; i < codeLength; i++)
+            {
+                // Find most common digit
+                int oneCount = 0;
+                int zeroCount = 0;
+
+                if (currentInput.Length == 1)
+                {
+                    break;
+                }
+
+                foreach (var code in currentInput)
+                {
+                    if (code[i] == '1') oneCount++;
+                    else zeroCount++;
+                }
+
+                currentInput = predicate(oneCount, zeroCount) ? currentInput.Where(l => l[i] == '1').ToArray() : currentInput.Where(l => l[i] == '0').ToArray();
+            }
+
+            return Convert.ToInt32(currentInput[0], 2);
+        }
+
+        public (int, int) CalculateOxygenAndCO2Rating(string[] input)
+        {
+            int oxygenRating = CalculateLifeSupportRating(input, (oneCount, zeroCount) => oneCount >= zeroCount);
+            int co2Rating = CalculateLifeSupportRating(input, (oneCount, zeroCount) => oneCount < zeroCount);
+
+            return (oxygenRating, co2Rating);
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            var rates = CalculateGammaAndEpsilonRate(_input);
+            var result = rates.Item1 * rates.Item2;
+
+            return new(result.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            var rates = CalculateOxygenAndCO2Rating(_input);
+            var result = rates.Item1 * rates.Item2;
+
+            return new(result.ToString());
+        }
     }
 }
