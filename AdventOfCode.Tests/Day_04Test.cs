1b11a777feb233390c456ac39af141020249a758
Miles Bardon <milesbardon@gmail.com>
2021-12-04 15:34:35 +0000

    Completing Day 4

--- AdventOfCode.Tests/Day_04Test.cs
+++ AdventOfCode.Tests/Day_04Test.cs
@@ -2,16 +2,119 @@ using Xunit;
 
 namespace AdventOfCode.Tests
 {
+    public class BingoCard_Should
+    {
+        string[] cardInput = new string[] {
+            "22 13 17 11  0",
+            "8  2 23  4 24",
+            "21  9 14 16  7",
+            "6 10  3 18  5",
+            "1 12 20 15 19"
+        };
+
+        [Fact]
+        public void MarkNumber_UpdatesCard()
+        {
+            BingoCard card = new BingoCard(cardInput);
+            card.MarkNumber(10);
+
+            Assert.Equal(10, card.card[3][1]);
+            Assert.True(card.marked[3][1]);
+        }
+
+        [Theory]
+        [InlineData(17, 23, 14, 3, 20)]
+        [InlineData(1, 12, 20, 15, 19)]
+        public void CheckLines_IsComplete_ReturnsLine(params int[] lineToCheck)
+        {
+            BingoCard card = new BingoCard(cardInput);
+
+            foreach (var value in lineToCheck)
+            {
+                card.MarkNumber(value);
+            }
+
+            Assert.Equal(5, card.CheckLines().Length);
+        }
+
+        [Theory]
+        [InlineData(17, 23, 14, 3, 8)]
+        [InlineData(1, 12, 15, 19, 24)]
+        public void CheckLines_IsNotComplete_ReturnsEmpty(params int[] lineToCheck)
+        {
+            BingoCard card = new BingoCard(cardInput);
+
+            foreach (var value in lineToCheck)
+            {
+                card.MarkNumber(value);
+            }
+
+            Assert.Empty(card.CheckLines());
+        }
+
+        [Fact]
+        public void UnmarkedNumbers_ReturnsAnswer()
+        {
+            BingoCard card = new BingoCard(cardInput);
+
+            int[] marked = new int[] {
+                22, 4, 9, 18, 5, 1, 20
+            };
+
+            foreach (var value in marked)
+            {
+                card.MarkNumber(value);
+            }
+
+            Assert.Equal(221, card.UnmarkedNumbers());
+        }
+    }
+
     public class Day_04_Should
     {
         Day_04 day = new Day_04();
+
+        string[] input = new string[] {
+            "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1",
+            "22 13 17 11  0",
+            " 8  2 23  4 24",
+            "21  9 14 16  7",
+            " 6 10  3 18  5",
+            " 1 12 20 15 19",
+            " 3 15  0  2 22",
+            " 9 18 13 17  5",
+            "19  8  7 25 23",
+            "20 11 10 24  4",
+            "14 21 16 12  6",
+            "14 21 17 24  4",
+            "10 16 15  9 19",
+            "18  8 23 26 20",
+            "22 11 13  6  5",
+            " 2  0 12  3  7"
+        };
+
+        [Fact]
+        public void Example1_ReturnsAnswer()
+        {
+            int result = day.PlayBingo(input);
+
+            Assert.Equal(4512, result);
+        }
+
+        [Fact]
+        public void Example2_ReturnsAnswer()
+        {
+            int result = day.PlayUntilLastBingoCard(input);
+
+            Assert.Equal(1924, result);
+        }
 
         [Fact]
         public void Solution1_ReturnsAnswer()
         {
             var result = day.Solve_1().ToString();
 
-            Assert.Equal("Solution 1", result);
+            Assert.Equal("44088", result);
         }
 
         [Fact]
@@ -19,7 +122,7 @@ namespace AdventOfCode.Tests
         {
             var result = day.Solve_2().ToString();
 
-            Assert.Equal("Solution 2", result);
+            Assert.Equal("23670", result);
         }
     }
 }
--- AdventOfCode/Day_04.cs
+++ AdventOfCode/Day_04.cs
@@ -2,17 +2,181 @@ using AoCHelper;
 
 namespace AdventOfCode
 {
+    public class BingoCard
+    {
+        public int[][] card { get; }
+        public bool[][] marked { get; }
+
+        public BingoCard(string[] input)
+        {
+            card = new int[5][];
+            marked = new bool[5][];
+
+            int i = 0;
+            foreach (string line in input)
+            {
+                string[] splitLine = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                int[] lineNumbers = Array.ConvertAll(splitLine, s => int.Parse(s.Trim()));
+
+                card.SetValue(lineNumbers, i);
+                marked.SetValue(new bool[5], i);
+                i++;
+            }
+        }
+
+        public void MarkNumber(int value)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (card[i][j] == value)
+                    {
+                        marked[i][j] = true;
+                        return;
+                    }
+                }
+            }
+        }
+
+        public int[] CheckLines()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                // Check rows first
+                if (marked[i].All(b => b)) return card[i];
+
+                bool[] col = new bool[5];
+                // Check each column
+                for (int j = 0; j < 5; j++)
+                {
+                    bool isMarked = marked[j][i];
+
+                    // Drop out early if one of the values isn't marked
+                    if (!isMarked) break;
+
+                    col[j] = isMarked;
+                }
+
+                if (col.Length == 5 && col.All(b => b))
+                {
+                    int[] colVals = new int[5];
+
+                    for (int k = 0; k < 5; k++)
+                    {
+                        colVals[k] = card[k][i];
+                    }
+
+                    return colVals;
+                }
+            }
+
+            return new int[0];
+        }
+
+        public int UnmarkedNumbers()
+        {
+            int result = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (!marked[i][j]) result += card[i][j];
+                }
+            }
+
+            return result;
+        }
+    }
+
     public class Day_04 : BaseDay
     {
-        private readonly string _input;
+        private readonly string[] _input;
 
         public Day_04()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = File.ReadAllText(InputFilePath).Split('\n').Where(l => l != "").ToArray();
         }
 
-        public override ValueTask<string> Solve_1() => new("Solution 1");
+        private int[] GetDrawOrder(string[] input) => Array.ConvertAll(input[0].Split(','), s => int.Parse(s));
 
-        public override ValueTask<string> Solve_2() => new("Solution 2");
+        private List<BingoCard> GetBingoCards(string[] input)
+        {
+            List<BingoCard> cards = new List<BingoCard>();
+
+            for (int i = 1; i < input.Length; i += 5)
+            {
+                cards.Add(new BingoCard(new ArraySegment<string>(input, i, 5).ToArray()));
+            }
+
+            return cards;
+        }
+
+        public int PlayBingo(string[] input)
+        {
+            int[] drawOrder = GetDrawOrder(input);
+            List<BingoCard> cards = GetBingoCards(input);
+
+            foreach (var draw in drawOrder)
+            {
+                foreach (var card in cards)
+                {
+                    card.MarkNumber(draw);
+                    var line = card.CheckLines();
+
+                    if (line.Length == 5)
+                    {
+                        return card.UnmarkedNumbers() * draw;
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public int PlayUntilLastBingoCard(string[] input)
+        {
+            int[] drawOrder = GetDrawOrder(input);
+            List<BingoCard> cards = GetBingoCards(input);
+
+            HashSet<BingoCard> winners = new HashSet<BingoCard>();
+
+            foreach (var draw in drawOrder)
+            {
+                foreach (var card in cards)
+                {
+                    card.MarkNumber(draw);
+                    var line = card.CheckLines();
+
+                    if (line.Length == 5)
+                    {
+                        // Card has now won
+                        winners.Add(card);
+
+                        if (winners.Count == cards.Count)
+                        {
+                            return winners.Last().UnmarkedNumbers() * draw;
+                        }
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            var result = PlayBingo(_input);
+
+            return new(result.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            var result = PlayUntilLastBingoCard(_input);
+
+            return new(result.ToString());
+        }
     }
 }
