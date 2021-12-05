bdf7a04823d91ed27f22f171461fdb0c4b4601da
Miles Bardon <milesbardon@gmail.com>
2021-12-05 13:12:37 +0000

    Completing Day 5

--- AdventOfCode.Tests/Day_05Test.cs
+++ AdventOfCode.Tests/Day_05Test.cs
@@ -2,16 +2,68 @@ using Xunit;
 
 namespace AdventOfCode.Tests
 {
+    public class OceanFloor_Should
+    {
+        [Fact]
+        public void MapLine_PlotsCorrectly()
+        {
+            string[] input = new string[] {
+                "0,9 -> 5,9", "6,4 -> 2,0"
+            };
+
+            OceanFloor floor = new OceanFloor(input, true);
+
+            var floorMap = floor.ventMap;
+
+            Assert.Equal(1, floorMap.TryGetValue(new Coords(0, 9), out var value) ? value : 0);
+            Assert.Equal(1, floorMap.TryGetValue(new Coords(1, 9), out var value1) ? value1 : 0);
+            Assert.Equal(1, floorMap.TryGetValue(new Coords(2, 9), out var value2) ? value2 : 0);
+            Assert.Equal(1, floorMap.TryGetValue(new Coords(3, 9), out var value3) ? value3 : 0);
+            Assert.Equal(1, floorMap.TryGetValue(new Coords(4, 9), out var value4) ? value4 : 0);
+            Assert.Equal(1, floorMap.TryGetValue(new Coords(5, 9), out var value5) ? value5 : 0);
+
+            Assert.Equal(0, floorMap.TryGetValue(new Coords(6, 4), out var value6) ? value6 : 0);
+        }
+    }
+
     public class Day_05_Should
     {
         Day_05 day = new Day_05();
+        string[] input = new string[] {
+            "0,9 -> 5,9",
+            "8,0 -> 0,8",
+            "9,4 -> 3,4",
+            "2,2 -> 2,1",
+            "7,0 -> 7,4",
+            "6,4 -> 2,0",
+            "0,9 -> 2,9",
+            "3,4 -> 1,4",
+            "0,0 -> 8,8",
+            "5,5 -> 8,2",
+        };
+
+        [Fact]
+        public void Example1_ReturnsAnswer()
+        {
+            OceanFloor floor = new OceanFloor(input, true);
+
+            Assert.Equal(5, floor.CountOverlappingVents());
+        }
+
+        [Fact]
+        public void Example2_ReturnsAnswer()
+        {
+            OceanFloor floor = new OceanFloor(input, false);
+
+            Assert.Equal(12, floor.CountOverlappingVents());
+        }
 
         [Fact]
         public void Solution1_ReturnsAnswer()
         {
             var result = day.Solve_1().ToString();
 
-            Assert.Equal("Solution 1", result);
+            Assert.Equal("5124", result);
         }
 
         [Fact]
@@ -19,7 +71,7 @@ namespace AdventOfCode.Tests
         {
             var result = day.Solve_2().ToString();
 
-            Assert.Equal("Solution 2", result);
+            Assert.Equal("19771", result);
         }
     }
 }
--- AdventOfCode/Day_05.cs
+++ AdventOfCode/Day_05.cs
@@ -2,17 +2,137 @@ using AoCHelper;
 
 namespace AdventOfCode
 {
+    public static class Sequence
+    {
+        public static IEnumerable<int> Range(int start, int end)
+        {
+            int current = start;
+            if (start > end)
+            {
+                while (current >= end)
+                {
+                    yield return current--;
+                }
+            }
+            else
+            {
+                while (current <= end)
+                {
+                    yield return current++;
+                }
+            }
+        }
+    }
+
+    public readonly struct Coords
+    {
+        public Coords(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; init; }
+        public int Y { get; init; }
+
+        public bool Equals(Coords other) => X == other.X && Y == other.Y;
+
+        public override int GetHashCode() => HashCode.Combine(X, Y);
+    }
+
+    public class OceanFloor
+    {
+        public Dictionary<Coords, int> ventMap { get; }
+
+        public OceanFloor(string[] input, bool horizontalOnly)
+        {
+            ventMap = new Dictionary<Coords, int>();
+
+            foreach (var line in input)
+            {
+                var coords = ParseInputLine(line, horizontalOnly);
+
+                foreach (var point in coords)
+                {
+                    var count = ventMap.TryGetValue(point, out var value) ? value + 1 : 1;
+                    if (count > 1) ventMap[point] = count;
+                    else ventMap.Add(point, count);
+                }
+            }
+        }
+
+        public int CountOverlappingVents() => ventMap.Values.Where(v => v > 1).Count();
+
+        private List<Coords> ParseInputLine(string line, bool horizontalOnly)
+        {
+            var parts = line.Split(" -> ");
+            var start = parts[0].Split(',');
+            var end = parts[1].Split(',');
+
+            var startX = int.Parse(start[0]);
+            var startY = int.Parse(start[1]);
+            var endX = int.Parse(end[0]);
+            var endY = int.Parse(end[1]);
+
+            var result = new List<Coords>();
+
+            // Ignore diagonals here
+            var isDiagonal = startX != endX && startY != endY;
+
+            if (isDiagonal)
+            {
+                if (horizontalOnly)
+                    return result;
+                else
+                {
+                    IEnumerable<int> xRange = Sequence.Range(startX, endX);
+                    IEnumerable<int> yRange = Sequence.Range(startY, endY);
+
+                    return xRange.Zip(yRange, (x, y) => new Coords(x, y)).ToList();
+                }
+            }
+
+            if (startX == endX)
+            {
+                IEnumerable<int> yRange = Sequence.Range(startY, endY);
+                IEnumerable<int> xRange = Enumerable.Repeat(startX, yRange.Count());
+
+                return xRange.Zip(yRange, (x, y) => new Coords(x, y)).ToList();
+            }
+
+            if (startY == endY)
+            {
+                IEnumerable<int> xRange = Sequence.Range(startX, endX);
+                IEnumerable<int> yRange = Enumerable.Repeat(startY, xRange.Count());
+
+                return xRange.Zip(yRange, (x, y) => new Coords(x, y)).ToList();
+            }
+
+            return result;
+        }
+    }
+
     public class Day_05 : BaseDay
     {
-        private readonly string _input;
+        private readonly string[] _input;
 
         public Day_05()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = File.ReadAllText(InputFilePath).Split('\n');
         }
 
-        public override ValueTask<string> Solve_1() => new("Solution 1");
+        public override ValueTask<string> Solve_1()
+        {
+            OceanFloor floor = new OceanFloor(_input, true);
 
-        public override ValueTask<string> Solve_2() => new("Solution 2");
+            return new(floor.CountOverlappingVents().ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            OceanFloor floor = new OceanFloor(_input, false);
+
+            return new(floor.CountOverlappingVents().ToString());
+        }
     }
 }
