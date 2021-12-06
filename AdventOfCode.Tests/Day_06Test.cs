1e2149a139f3f852e3e9d76b6afe3b01f3f4080a
Miles Bardon <milesbardon@gmail.com>
2021-12-06 19:33:28 +0000

    Completing Day 6

--- AdventOfCode.Tests/Day_06Test.cs
+++ AdventOfCode.Tests/Day_06Test.cs
@@ -5,13 +5,31 @@ namespace AdventOfCode.Tests
     public class Day_06_Should
     {
         Day_06 day = new Day_06();
+        int[] input = new int[] { 3, 4, 3, 1, 2 };
+
+        [Fact]
+        public void Example1_ReturnsAnswer()
+        {
+            var result18 = day.SimulateFish(input, 18);
+            var result80 = day.SimulateFish(input, 80);
+
+            Assert.Equal(26, result18);
+            Assert.Equal(5934, result80);
+        }
+
+        public void Example2_ReturnsAnswer()
+        {
+            var result = day.SimulateFish(input, 256);
+
+            Assert.Equal(26984457539, result);
+        }
 
         [Fact]
         public void Solution1_ReturnsAnswer()
         {
             var result = day.Solve_1().ToString();
 
-            Assert.Equal("Solution 1", result);
+            Assert.Equal("363101", result);
         }
 
         [Fact]
@@ -19,7 +37,7 @@ namespace AdventOfCode.Tests
         {
             var result = day.Solve_2().ToString();
 
-            Assert.Equal("Solution 2", result);
+            Assert.Equal("1644286074024", result);
         }
     }
 }
--- AdventOfCode/Day_06.cs
+++ AdventOfCode/Day_06.cs
@@ -4,15 +4,49 @@ namespace AdventOfCode
 {
     public class Day_06 : BaseDay
     {
-        private readonly string _input;
+        private readonly int[] _input;
 
         public Day_06()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = Array.ConvertAll(File.ReadAllText(InputFilePath).Split(','), s => int.Parse(s));
         }
 
-        public override ValueTask<string> Solve_1() => new("Solution 1");
+        public long SimulateFish(IEnumerable<int> initialFish, int days)
+        {
+            const int cycleLength = 7;
+            const int delayPeriod = 2;
 
-        public override ValueTask<string> Solve_2() => new("Solution 2");
+            // Create a "bucket" of fish cycle lengths
+            var fish = new Dictionary<int, long>(Enumerable.Range(0, cycleLength).Select(i => new KeyValuePair<int, long>(i, 0)));
+
+            // Group fish by their initial cycle length
+            foreach (var day in initialFish.GroupBy(f => f))
+            {
+                fish[day.Key] = day.Count();
+            }
+
+            var queue = new Queue<long>(Enumerable.Range(0, delayPeriod).Select(_ => 0L));
+
+            for (var i = 0; i < days; i++)
+            {
+                // Since it's a cycle, each day just progresses it by one
+                var spawnersIndex = i % cycleLength;
+
+                // Get the number of fish that will spawn and place them in the queue
+                queue.Enqueue(fish[spawnersIndex]);
+
+                // Remove the previous spawners from the queue and use them to create the new fish
+                var juveniles = queue.Dequeue();
+
+                // The new fish are added to the next cycle bucket (ignoring the 8th day)
+                fish[(spawnersIndex + cycleLength) % cycleLength] += juveniles;
+            }
+
+            return fish.Sum(d => d.Value) + queue.Sum();
+        }
+
+        public override ValueTask<string> Solve_1() => new(SimulateFish(_input, 80).ToString());
+
+        public override ValueTask<string> Solve_2() => new(SimulateFish(_input, 256).ToString());
     }
 }
