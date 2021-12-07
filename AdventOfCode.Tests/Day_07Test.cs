234f4731cdc287ef1f95541c40b42a060c38a363
Miles Bardon <milesbardon@gmail.com>
2021-12-07 10:35:18 +0000

    Completing Day 7 Part 1

--- AdventOfCode.Tests/Day_07Test.cs
+++ AdventOfCode.Tests/Day_07Test.cs
@@ -5,13 +5,30 @@ namespace AdventOfCode.Tests
     public class Day_07_Should
     {
         Day_07 day = new Day_07();
+        int[] input = new int[] { 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 };
+
+        [Fact]
+        public void Example1_ReturnsAnswer()
+        {
+            var result = day.CheapestFuel(input);
+
+            Assert.Equal(37, result);
+        }
+
+        [Fact]
+        public void Example2_ReturnsAnswer()
+        {
+            var result = day.CheapestFuelAvg(input);
+
+            Assert.Equal(168, result);
+        }
 
         [Fact]
         public void Solution1_ReturnsAnswer()
         {
             var result = day.Solve_1().ToString();
 
-            Assert.Equal("Solution 1", result);
+            Assert.Equal("341534", result);
         }
 
         [Fact]
--- AdventOfCode/Day_07.cs
+++ AdventOfCode/Day_07.cs
@@ -4,15 +4,51 @@ namespace AdventOfCode
 {
     public class Day_07 : BaseDay
     {
-        private readonly string _input;
+        private readonly int[] _input;
 
         public Day_07()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = Array.ConvertAll(File.ReadAllText(InputFilePath).Split(','), s => int.Parse(s));
         }
 
-        public override ValueTask<string> Solve_1() => new("Solution 1");
+        public int CheapestFuel(int[] input)
+        {
+            Array.Sort(input);
 
-        public override ValueTask<string> Solve_2() => new("Solution 2");
+            int middle = input.Count() / 2;
+            int median = input[middle];
+
+            int total = 0;
+            foreach (var crab in input)
+            {
+                total += Math.Abs(crab - median);
+            }
+
+            return total;
+        }
+
+        public int CheapestFuelAvg(int[] input)
+        {
+            double a = ((double)input.Sum() / (double)input.Count());
+            int avg = ((int)Math.Round(a));
+
+            int total = 0;
+            foreach (var crab in input)
+            {
+                int subtotal = 0;
+                for (int i = 1; i <= (Math.Abs(crab - avg)); i++)
+                {
+                    subtotal += i;
+                }
+
+                total += subtotal;
+            }
+
+            return total;
+        }
+
+        public override ValueTask<string> Solve_1() => new(CheapestFuel(_input).ToString());
+
+        public override ValueTask<string> Solve_2() => new(CheapestFuelAvg(_input).ToString());
     }
 }
