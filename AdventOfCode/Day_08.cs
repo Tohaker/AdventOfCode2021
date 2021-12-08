6ea59819c9caf7c92f9c219b5d051760d2250f28
Miles Bardon <milesbardon@gmail.com>
2021-12-08 09:23:55 +0000

    Completing Day 8 part 1

--- AdventOfCode/Day_08.cs
+++ AdventOfCode/Day_08.cs
@@ -4,14 +4,34 @@ namespace AdventOfCode
 {
     public class Day_08 : BaseDay
     {
-        private readonly string _input;
+        private readonly string[] _input;
 
         public Day_08()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = File.ReadAllText(InputFilePath).Split('\n');
         }
 
-        public override ValueTask<string> Solve_1() => new("Solution 1");
+        private (string[], string[]) ParseLine(string line)
+        {
+            var parts = line.Split(" | ");
+
+            return (parts[0].Split(), parts[1].Split());
+        }
+
+        public int CountUniqueDigits(string[] input)
+        {
+            int count = 0;
+            int[] uniqueDigitLengths = new int[] { 2, 3, 4, 7 };
+
+            foreach (var line in input)
+            {
+                count += ParseLine(line).Item2.Where(s => uniqueDigitLengths.Contains(s.Length)).Count();
+            }
+
+            return count;
+        }
+
+        public override ValueTask<string> Solve_1() => new(CountUniqueDigits(_input).ToString());
 
         public override ValueTask<string> Solve_2() => new("Solution 2");
     }
