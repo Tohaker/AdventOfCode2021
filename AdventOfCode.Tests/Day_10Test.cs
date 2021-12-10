f7c79f7c97dc4009ced7f741b969dc634c36fc03
Miles Bardon <milesbardon@gmail.com>
2021-12-10 21:09:51 +0000

    Completing Day 10

--- AdventOfCode.Tests/Day_10Test.cs
+++ AdventOfCode.Tests/Day_10Test.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 
 namespace AdventOfCode.Tests
@@ -5,13 +6,58 @@ namespace AdventOfCode.Tests
     public class Day_10_Should
     {
         Day_10 day = new Day_10();
+        string[] input = new string[] {
+            "[({(<(())[]>[[{[]{<()<>>",
+            "[(()[<>])]({[<{<<[]>>(",
+            "{([(<{}[<>[]}>{[]{[(<()>",
+            "(((({<>}<{<{<>}{[]{[]{}",
+            "[[<[([]))<([[{}[[()]]]",
+            "[{[{({}]{}}([{[{{{}}([]",
+            "{<[[]]>}<{[{[{[]{()[[[]",
+            "[<(<(<(<{}))><([]([]()",
+            "<{([([[(<>()){}]>(<<{{",
+            "<{([{{}}[<[[[<>{}]]]>[]]",
+        };
+
+        [Fact]
+        public void FindFirstIllegalCharacter_ReturnsAnswer()
+        {
+            var result = day.FindFirstIllegalCharacter(input[2]);
+
+            Assert.Equal('}', result);
+        }
+
+        [Fact]
+        public void Example1_ReturnsAnswer()
+        {
+            var result = day.CountIllegalCharacters(input);
+
+            Assert.Equal(26397, result);
+        }
+
+        [Fact]
+        public void CompleteUnfinishedLine_ReturnsAnswer()
+        {
+            var result = day.CompleteUnfinishedLine(input[0]);
+            var expected = new List<char>() { '}', '}', ']', ']', ')', '}', ')', ']' };
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Example2_ReturnsAnswer()
+        {
+            var result = day.CountAutocompletedCharacters(input);
+
+            Assert.Equal(288957, result);
+        }
 
         [Fact]
         public void Solution1_ReturnsAnswer()
         {
             var result = day.Solve_1().ToString();
 
-            Assert.Equal("Solution 1", result);
+            Assert.Equal("392043", result);
         }
 
         [Fact]
@@ -19,7 +65,7 @@ namespace AdventOfCode.Tests
         {
             var result = day.Solve_2().ToString();
 
-            Assert.Equal("Solution 2", result);
+            Assert.Equal("1605968119", result);
         }
     }
 }
--- AdventOfCode/Day_10.cs
+++ AdventOfCode/Day_10.cs
@@ -4,15 +4,161 @@ namespace AdventOfCode
 {
     public class Day_10 : BaseDay
     {
-        private readonly string _input;
+        private readonly string[] _input;
 
         public Day_10()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = File.ReadAllText(InputFilePath).Split('\n');
         }
 
-        public override ValueTask<string> Solve_1() => new("Solution 1");
+        public char FindFirstIllegalCharacter(string line)
+        {
+            Stack<char> stack = new Stack<char>();
+            char[] closingChars = new char[] { ')', '}', ']', '>' };
 
-        public override ValueTask<string> Solve_2() => new("Solution 2");
+            foreach (char c in line.ToCharArray())
+            {
+                if (!closingChars.Contains(c))
+                {
+                    stack.Push(c);
+                    continue;
+                }
+
+                var lastOpened = stack.Peek();
+
+                switch (lastOpened)
+                {
+                    case '(':
+                        if (c == ')') stack.Pop();
+                        else return c;
+                        break;
+                    case '[':
+                        if (c == ']') stack.Pop();
+                        else return c;
+                        break;
+                    case '{':
+                        if (c == '}') stack.Pop();
+                        else return c;
+                        break;
+                    case '<':
+                        if (c == '>') stack.Pop();
+                        else return c;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return 'a';
+        }
+
+        public List<char> CompleteUnfinishedLine(string line)
+        {
+            Stack<char> stack = new Stack<char>();
+            char[] closingChars = new char[] { ')', '}', ']', '>' };
+            Dictionary<char, char> correspondingChars = new Dictionary<char, char>() {
+                {'(', ')'},
+                {'{', '}'},
+                {'[', ']'},
+                {'<', '>'}
+            };
+
+            foreach (char c in line.ToCharArray())
+            {
+                if (!closingChars.Contains(c))
+                {
+                    stack.Push(c);
+                    continue;
+                }
+
+                var lastOpened = stack.Peek();
+
+                switch (lastOpened)
+                {
+                    case '(':
+                        if (c == ')') stack.Pop();
+                        break;
+                    case '[':
+                        if (c == ']') stack.Pop();
+                        break;
+                    case '{':
+                        if (c == '}') stack.Pop();
+                        break;
+                    case '<':
+                        if (c == '>') stack.Pop();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            List<char> result = new List<char>();
+
+            foreach (char c in stack.AsEnumerable())
+            {
+                result.Add(correspondingChars[c]);
+            }
+
+            return result;
+        }
+
+        public int CountIllegalCharacters(string[] input)
+        {
+            int count = 0;
+
+            Dictionary<char, int> scores = new Dictionary<char, int>() {
+                {')', 3},
+                {']', 57},
+                {'}', 1197},
+                {'>', 25137},
+                {'a', 0}
+            };
+
+            foreach (var line in input)
+            {
+                count += scores[FindFirstIllegalCharacter(line)];
+            }
+
+            return count;
+        }
+
+        public long CountAutocompletedCharacters(string[] input)
+        {
+            List<long> counts = new List<long>();
+
+            Dictionary<char, int> scores = new Dictionary<char, int>() {
+                {')', 1},
+                {']', 2},
+                {'}', 3},
+                {'>', 4},
+            };
+
+            foreach (var line in input)
+            {
+                var c = FindFirstIllegalCharacter(line);
+                long count = 0;
+
+                if (c == 'a')
+                {
+                    var chars = CompleteUnfinishedLine(line);
+
+                    foreach (var ch in chars)
+                    {
+                        count *= 5;
+                        count += scores[ch];
+                    }
+                }
+
+                if (count > 0) counts.Add(count);
+            }
+
+            counts.Sort();
+
+            return counts[(counts.Count() / 2)];
+        }
+
+        public override ValueTask<string> Solve_1() => new(CountIllegalCharacters(_input).ToString());
+
+        public override ValueTask<string> Solve_2() => new(CountAutocompletedCharacters(_input).ToString());
     }
 }
