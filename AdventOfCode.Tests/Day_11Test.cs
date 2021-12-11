db1890ac8666a992e51b4f094c203d5319ed6fe9
Miles Bardon <milesbardon@gmail.com>
2021-12-11 15:09:56 +0000

    Completing Day 11

--- AdventOfCode.Tests/Day_11Test.cs
+++ AdventOfCode.Tests/Day_11Test.cs
@@ -5,13 +5,40 @@ namespace AdventOfCode.Tests
     public class Day_11_Should
     {
         Day_11 day = new Day_11();
+        string[] input = new string[] {
+            "5483143223",
+            "2745854711",
+            "5264556173",
+            "6141336146",
+            "6357385478",
+            "4167524645",
+            "2176841721",
+            "6882881134",
+            "4846848554",
+            "5283751526",
+        };
+
+        [Fact]
+        public void Example1_ReturnsAnswer()
+        {
+            var result = day.CountFlashes(input, 100);
+
+            Assert.Equal(1656, result);
+        }
+
+        public void Example2_ReturnsAnswer()
+        {
+            var result = day.FindSimultaneousFlashes(input);
+
+            Assert.Equal(195, result);
+        }
 
         [Fact]
         public void Solution1_ReturnsAnswer()
         {
             var result = day.Solve_1().ToString();
 
-            Assert.Equal("Solution 1", result);
+            Assert.Equal("1723", result);
         }
 
         [Fact]
@@ -19,7 +46,7 @@ namespace AdventOfCode.Tests
         {
             var result = day.Solve_2().ToString();
 
-            Assert.Equal("Solution 2", result);
+            Assert.Equal("327", result);
         }
     }
 }
--- AdventOfCode/Day_11.cs
+++ AdventOfCode/Day_11.cs
@@ -1,18 +1,216 @@
 using AoCHelper;
+using System.Numerics;
 
 namespace AdventOfCode
 {
     public class Day_11 : BaseDay
     {
-        private readonly string _input;
+        private readonly string[] _input;
 
         public Day_11()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = File.ReadAllText(InputFilePath).Split('\n');
         }
 
-        public override ValueTask<string> Solve_1() => new("Solution 1");
+        private List<List<int>> GetEnergyLevels(string[] input)
+        {
+            List<List<int>> energyLevels = new List<List<int>>();
 
-        public override ValueTask<string> Solve_2() => new("Solution 2");
+            foreach (var line in input)
+            {
+                energyLevels.Add(Array.ConvertAll(line.ToCharArray(), c => c - '0').ToList());
+            }
+
+            return energyLevels;
+        }
+
+        private void RaiseEnergyLevels(ref List<List<int>> octopi, int value)
+        {
+            for (int i = 0; i < octopi.Count(); i++)
+            {
+                var row = octopi[i];
+                for (int j = 0; j < row.Count(); j++)
+                {
+                    octopi[i][j]++;
+                }
+            }
+        }
+
+        private int FlashOctopi(ref List<List<int>> octopi, int threshold)
+        {
+            int flashCount = 0;
+            int prevFlashCount = -1;
+
+            List<Vector2> alreadyFlashed = new List<Vector2>();
+
+            var rowCount = octopi.Count() - 1;
+            var rowLength = octopi[0].Count() - 1;
+
+            while (flashCount != prevFlashCount)
+            {
+                prevFlashCount = flashCount;
+                for (int i = 0; i <= rowCount; i++)
+                {
+                    var row = octopi[i];
+                    for (int j = 0; j <= rowLength; j++)
+                    {
+                        if (row[j] > threshold)
+                        {
+                            // This octopus flashes (how to check if flashed only once?)
+                            if (alreadyFlashed.Contains(new Vector2(j, i))) continue;
+
+                            flashCount++;
+                            alreadyFlashed.Add(new Vector2(j, i));
+
+                            // Raise value of surrounding octopi
+                            if (i == 0)
+                            {
+                                if (j == 0)
+                                {
+                                    octopi[i][j + 1]++;
+                                    octopi[i + 1][j]++;
+                                    octopi[i + 1][j + 1]++;
+                                }
+                                else if (j == rowLength)
+                                {
+                                    octopi[i][j - 1]++;
+                                    octopi[i + 1][j]++;
+                                    octopi[i + 1][j - 1]++;
+                                }
+                                else
+                                {
+                                    octopi[i][j + 1]++;
+                                    octopi[i][j - 1]++;
+
+                                    octopi[i + 1][j]++;
+                                    octopi[i + 1][j + 1]++;
+                                    octopi[i + 1][j - 1]++;
+                                }
+                            }
+                            else if (i == rowCount)
+                            {
+                                if (j == 0)
+                                {
+                                    octopi[i][j + 1]++;
+                                    octopi[i - 1][j]++;
+                                    octopi[i - 1][j + 1]++;
+                                }
+                                else if (j == rowLength)
+                                {
+                                    octopi[i][j - 1]++;
+                                    octopi[i - 1][j]++;
+                                    octopi[i - 1][j - 1]++;
+                                }
+                                else
+                                {
+                                    octopi[i][j + 1]++;
+                                    octopi[i][j - 1]++;
+
+                                    octopi[i - 1][j]++;
+                                    octopi[i - 1][j + 1]++;
+                                    octopi[i - 1][j - 1]++;
+                                }
+                            }
+                            else
+                            {
+
+                                octopi[i - 1][j]++;
+                                octopi[i + 1][j]++;
+
+                                if (j == 0)
+                                {
+                                    octopi[i][j + 1]++;
+                                    octopi[i - 1][j + 1]++;
+                                    octopi[i + 1][j + 1]++;
+                                }
+                                else if (j == rowLength)
+                                {
+                                    octopi[i][j - 1]++;
+                                    octopi[i - 1][j - 1]++;
+                                    octopi[i + 1][j - 1]++;
+                                }
+                                else
+                                {
+                                    octopi[i][j - 1]++;
+                                    octopi[i - 1][j - 1]++;
+                                    octopi[i + 1][j - 1]++;
+
+                                    octopi[i][j + 1]++;
+                                    octopi[i - 1][j + 1]++;
+                                    octopi[i + 1][j + 1]++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (var octo in alreadyFlashed)
+            {
+                octopi[(int)octo.Y][(int)octo.X] = 0;
+            }
+
+            return flashCount;
+        }
+
+        private void DebugOctopi(ref List<List<int>> octopi)
+        {
+            string output = "";
+
+            foreach (var row in octopi)
+            {
+                output += (String.Join(' ', Array.ConvertAll(row.ToArray(), i => i.ToString()))) + '\n';
+            }
+
+            Console.WriteLine(output);
+        }
+
+        private int SumAllEnergyLevels(ref List<List<int>> octopi)
+        {
+            int count = 0;
+
+            foreach (var row in octopi)
+            {
+                count += row.Sum();
+            }
+
+            return count;
+        }
+
+        public int CountFlashes(string[] input, int steps)
+        {
+            int count = 0;
+
+            var energyLevels = GetEnergyLevels(input);
+
+            for (int i = 0; i < steps; i++)
+            {
+                // DebugOctopi(ref energyLevels);
+                RaiseEnergyLevels(ref energyLevels, 1);
+                count += FlashOctopi(ref energyLevels, 9);
+            }
+
+            return count;
+        }
+
+        public int FindSimultaneousFlashes(string[] input)
+        {
+            int i = 0;
+
+            var energyLevels = GetEnergyLevels(input);
+
+            while (SumAllEnergyLevels(ref energyLevels) != 0)
+            {
+                RaiseEnergyLevels(ref energyLevels, 1);
+                FlashOctopi(ref energyLevels, 9);
+                i++;
+            }
+
+            return i;
+        }
+
+        public override ValueTask<string> Solve_1() => new(CountFlashes(_input, 100).ToString());
+
+        public override ValueTask<string> Solve_2() => new(FindSimultaneousFlashes(_input).ToString());
     }
 }
