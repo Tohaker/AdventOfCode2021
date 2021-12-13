f2f915b1d7da0c5fa9b4bee637d4d9576f13ce59
Miles Bardon <milesbardon@gmail.com>
2021-12-13 18:12:23 +0000

    Completing Day 13

--- AdventOfCode.Tests/Day_13Test.cs
+++ AdventOfCode.Tests/Day_13Test.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Xunit;
 
 namespace AdventOfCode.Tests
@@ -5,21 +6,53 @@ namespace AdventOfCode.Tests
     public class Day_13_Should
     {
         Day_13 day = new Day_13();
+        string[] input = new string[] {
+            "6,10",
+            "0,14",
+            "9,10",
+            "0,3",
+            "10,4",
+            "4,11",
+            "6,0",
+            "6,12",
+            "4,1",
+            "0,13",
+            "10,12",
+            "3,4",
+            "3,0",
+            "8,4",
+            "1,10",
+            "2,14",
+            "8,10",
+            "9,0",
+            "",
+            "fold along y=7",
+            "fold along x=5",
+        };
+
+        [Fact]
+        public void Example1_ReturnsAnswer()
+        {
+            var dots = day.FoldPaper(input, 1);
+
+            Assert.Equal(17, dots.Count);
+        }
+
+        [Fact]
+        public void Example2_ReturnsAnswer()
+        {
+            var output = day.PrintDots(day.FoldPaper(input));
+            var expected = "#####\n#   #\n#   #\n#   #\n#####\n";
+
+            Assert.Equal(expected, output);
+        }
 
         [Fact]
         public void Solution1_ReturnsAnswer()
         {
             var result = day.Solve_1().ToString();
 
-            Assert.Equal("Solution 1", result);
-        }
-
-        [Fact]
-        public void Solution2_ReturnsAnswer()
-        {
-            var result = day.Solve_2().ToString();
-
-            Assert.Equal("Solution 2", result);
+            Assert.Equal("743", result);
         }
     }
 }
--- AdventOfCode/Day_13.cs
+++ AdventOfCode/Day_13.cs
@@ -1,18 +1,97 @@
 using AoCHelper;
+using System.Numerics;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode
 {
     public class Day_13 : BaseDay
     {
-        private readonly string _input;
+        private readonly string[] _input;
 
         public Day_13()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = File.ReadAllText(InputFilePath).Split('\n');
         }
 
-        public override ValueTask<string> Solve_1() => new("Solution 1");
+        public HashSet<Vector2> FoldPaper(string[] input, int folds = 0)
+        {
+            HashSet<Vector2> dots = new();
+            List<(string, int)> foldList = new();
 
-        public override ValueTask<string> Solve_2() => new("Solution 2");
+            foreach (var line in input)
+            {
+                if (line == "") continue;
+
+                if (Regex.IsMatch(line, @"^\d"))
+                {
+                    var parts = line.Split(',');
+                    dots.Add(new Vector2(int.Parse(parts[0]), int.Parse(parts[1])));
+                }
+                else
+                {
+                    var parts = line.Split("fold along ")[1].Split('=');
+                    foldList.Add((parts[0], int.Parse(parts[1])));
+                }
+            }
+
+            var foldListSubset = folds == 0 ? foldList : foldList.Take(folds);
+
+            foreach (var (axis, value) in foldListSubset)
+            {
+                if (axis == "x")
+                {
+                    var pointsRightOfLine = dots.Where(v => v.X > value).ToArray();
+                    foreach (var point in pointsRightOfLine)
+                    {
+                        var newX = point.X - 2 * (point.X - value);
+                        dots.Add(new Vector2(newX, point.Y));
+                        dots.RemoveWhere(v => v == point);
+                    }
+                }
+                else
+                {
+                    var pointsAboveLine = dots.Where(v => v.Y > value).ToArray();
+                    foreach (var point in pointsAboveLine)
+                    {
+                        var newY = point.Y - 2 * (point.Y - value);
+                        dots.Add(new Vector2(point.X, newY));
+                        dots.RemoveWhere(v => v == point);
+                    }
+                }
+            }
+
+            return dots;
+        }
+
+        public string PrintDots(HashSet<Vector2> dots)
+        {
+            var list = dots.ToList();
+            var greatestX = dots.OrderBy(v => v.X).Last().X;
+            var greatestY = dots.OrderBy(v => v.Y).Last().Y;
+
+            string result = "";
+
+            for (int y = 0; y <= greatestY; y++)
+            {
+                for (int x = 0; x <= greatestX; x++)
+                {
+                    if (dots.Contains(new Vector2(x, y))) result += "#";
+                    else result += " ";
+                }
+
+                result += '\n';
+            }
+
+            return result;
+        }
+
+        public override ValueTask<string> Solve_1() => new(FoldPaper(_input, 1).Count.ToString());
+
+        public override ValueTask<string> Solve_2()
+        {
+            Console.WriteLine(PrintDots(FoldPaper(_input)));
+
+            return new("Solution 2");
+        }
     }
 }
