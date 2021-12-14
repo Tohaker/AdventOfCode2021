395f6002bda396244c9715b567911dc7034883df
Miles Bardon <milesbardon@gmail.com>
2021-12-14 17:32:12 +0000

    Completing Day 14

--- AdventOfCode.Tests/Day_14Test.cs
+++ AdventOfCode.Tests/Day_14Test.cs
@@ -5,13 +5,49 @@ namespace AdventOfCode.Tests
     public class Day_14_Should
     {
         Day_14 day = new Day_14();
+        string[] input = new string[] {
+            "NNCB",
+            "",
+            "CH -> B",
+            "HH -> N",
+            "CB -> H",
+            "NH -> C",
+            "HB -> C",
+            "HC -> B",
+            "HN -> C",
+            "NN -> C",
+            "BH -> H",
+            "NC -> B",
+            "NB -> B",
+            "BN -> B",
+            "BB -> N",
+            "BC -> B",
+            "CC -> N",
+            "CN -> C",
+        };
+
+        [Fact]
+        public void Example1_ReturnsAnswer()
+        {
+            var result = day.FindElementQuantities(input, 10);
+
+            Assert.Equal(1588, result);
+        }
+
+        [Fact]
+        public void Example2_ReturnsAnswer()
+        {
+            var result = day.FindElementQuantities(input, 40);
+
+            Assert.Equal(2188189693529, result);
+        }
 
         [Fact]
         public void Solution1_ReturnsAnswer()
         {
             var result = day.Solve_1().ToString();
 
-            Assert.Equal("Solution 1", result);
+            Assert.Equal("3411", result);
         }
 
         [Fact]
@@ -19,7 +55,7 @@ namespace AdventOfCode.Tests
         {
             var result = day.Solve_2().ToString();
 
-            Assert.Equal("Solution 2", result);
+            Assert.Equal("7477815755570", result);
         }
     }
 }
--- AdventOfCode/Day_14.cs
+++ AdventOfCode/Day_14.cs
@@ -1,18 +1,50 @@
 using AoCHelper;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode
 {
     public class Day_14 : BaseDay
     {
-        private readonly string _input;
+        private readonly string[] _input;
 
         public Day_14()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = File.ReadAllText(InputFilePath).Split('\n');
         }
 
-        public override ValueTask<string> Solve_1() => new("Solution 1");
+        public long FindElementQuantities(string[] input, int steps)
+        {
+            var template = input[0];
+            var rules = input.Skip(2).Select(l => l.Split(" -> ")).ToDictionary(r => r[0], r => r[1][0]);
 
-        public override ValueTask<string> Solve_2() => new("Solution 2");
+            // Find how many of each pair is in the template
+            var pairs = rules.ToDictionary(r => r.Key, r => new Regex(r.Key).Matches(template).LongCount());
+
+            // Count how many of each character is currently in the template
+            var chars = Enumerable.Range(0, 26).Select(i => (char)('A' + i)).ToDictionary(c => c, c => template.LongCount(t => t == c));
+
+
+            for (int i = 0; i < steps; i++)
+            {
+                // Include the count of values already in the template
+                foreach (var rule in rules)
+                    chars[rule.Value] += pairs[rule.Key];
+
+                pairs = rules.ToDictionary(r => r.Key,
+                    r => rules
+                    // Add the rule's value in the middle of itself, and check if it contains itself at all
+                    .Where(x => x.Key.Insert(1, x.Value.ToString()).Contains(r.Key))
+                    // The value of each rule is the total number of pairs already counted
+                    .Sum(x => pairs[x.Key]));
+            }
+
+            var elements = chars.Where(c => c.Value > 0).OrderBy(d => d.Value);
+
+            return elements.Last().Value - elements.First().Value;
+        }
+
+        public override ValueTask<string> Solve_1() => new(FindElementQuantities(_input, 10).ToString());
+
+        public override ValueTask<string> Solve_2() => new(FindElementQuantities(_input, 40).ToString());
     }
 }
