e0ba9146088abbafbbef29fe281e326aa1255783
Miles Bardon <milesbardon@gmail.com>
2021-12-17 21:06:40 +0000

    Completing Day 17 Part 1

--- AdventOfCode.Tests/Day_17Test.cs
+++ AdventOfCode.Tests/Day_17Test.cs
@@ -5,13 +5,22 @@ namespace AdventOfCode.Tests
     public class Day_17_Should
     {
         Day_17 day = new Day_17();
+        string input = "target area: x=20..30, y=-10..-5";
+
+        [Fact]
+        public void Example1_ReturnsAnswer()
+        {
+            var result = day.FindHighestY(input);
+
+            Assert.Equal(45, result);
+        }
 
         [Fact]
         public void Solution1_ReturnsAnswer()
         {
             var result = day.Solve_1().ToString();
 
-            Assert.Equal("Solution 1", result);
+            Assert.Equal("17766", result);
         }
 
         [Fact]
--- AdventOfCode/Day_17.cs
+++ AdventOfCode/Day_17.cs
@@ -1,4 +1,5 @@
 using AoCHelper;
+using System.Numerics;
 
 namespace AdventOfCode
 {
@@ -11,7 +12,19 @@ namespace AdventOfCode
             _input = File.ReadAllText(InputFilePath);
         }
 
-        public override ValueTask<string> Solve_1() => new("Solution 1");
+        public int FindHighestY(string input)
+        {
+            // Parse input
+            var parts = input.Remove(0, 13).Split(", ");
+            var yRange = parts[1].Remove(0, 2).Split("..");
+
+            var yStart = int.Parse(yRange[0]);
+            var yEnd = int.Parse(yRange[1]);
+
+            return Enumerable.Range(1, Math.Abs(Math.Min(yStart, yEnd)) - 1).Sum();
+        }
+
+        public override ValueTask<string> Solve_1() => new(FindHighestY(_input).ToString());
 
         public override ValueTask<string> Solve_2() => new("Solution 2");
     }
